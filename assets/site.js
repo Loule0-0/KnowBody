4a278c04e4f0c33a20cc5affd8b19863202329d6
Zeyu Lou <zeyu.lou.mail@gmail.com>
2026-09-21 22:40:46 +0800

    Redesign project page as an academic paper site

--- assets/site.js
+++ assets/site.js
@@ -1,18 +1,3 @@
-const navToggle = document.querySelector('.nav-toggle');
-const siteNav = document.querySelector('.site-nav');
-
-navToggle?.addEventListener('click', () => {
-  const isOpen = siteNav.classList.toggle('open');
-  navToggle.setAttribute('aria-expanded', String(isOpen));
-});
-
-siteNav?.querySelectorAll('a').forEach((link) => {
-  link.addEventListener('click', () => {
-    siteNav.classList.remove('open');
-    navToggle?.setAttribute('aria-expanded', 'false');
-  });
-});
-
 const dialog = document.querySelector('#figure-dialog');
 const dialogImage = dialog?.querySelector('img');
 
